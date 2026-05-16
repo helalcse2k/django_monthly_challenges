07a25c1ed9ec2ee8e9fb04f7a210ff611c903c47
M Helal Uddin <m.helal@echologyx.com>
2026-05-16 16:00:10 +0600

    dynamic urls added to templates

--- challenges/views.py
+++ challenges/views.py
@@ -22,17 +22,12 @@ monthly_challenges = {
 
 
 def index(request):
-    list_items = ""
+
     months = list(monthly_challenges.keys())
-    for month in months:
-        capalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f'<li><a href="{month_path}">{capalized_month}</a></li>'
 
-    # "<li><a href="/challenges/january">January</a></li> <li><a href="/challenges/february">February</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge_by_number(request, month):
